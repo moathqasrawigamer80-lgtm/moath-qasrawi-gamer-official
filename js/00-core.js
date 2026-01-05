3170499804a46960b5321e44c88b0e793cf8625a
moathqasrawigamer80-lgtm <moathqasrawigamer80@gmail.com>
2026-01-05 11:57:27 +0300

    Update 00-core.js

--- js/00-core.js
+++ js/00-core.js
@@ -1,1 +1,10 @@
+const App = {
+  qs: (e) => document.querySelector(e),
+  qsa: (e) => document.querySelectorAll(e),
+
+  storage: {
+    get: (k) => JSON.parse(localStorage.getItem(k)),
+    set: (k, v) => localStorage.setItem(k, JSON.stringify(v))
+  }
+};
 
